75b236a509849867c70a1636f396c521c016ee27
kizitech <kizitoohani@gmail.com>
2023-10-25 15:29:39 +0100

    add banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { BannerComponent } from './shared/components/banner/banner.component';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { NavbarComponent } from "./shared/components/navbar/navbar.component";
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        NavbarComponent
+        NavbarComponent,
+        BannerComponent,
     ]
 })
 export class AppModule { }
