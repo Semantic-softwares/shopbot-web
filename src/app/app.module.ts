877317b2252e8f3cbab8feac03b5756c59db3023
Travisken <123268134+Travisken@users.noreply.github.com>
2023-10-25 14:35:35 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { StoreCardComponent } from './shared/components/store-card/store-card.component';
-import { CategoryCardComponent } from './shared/components/category-card/category-card.component';
 import { FiltersComponent } from './shared/components/filters/filters.component';
 
 @NgModule({
-  declarations: [AppComponent, StoreCardComponent, CategoryCardComponent],
+  declarations: [
+    AppComponent
+  ],
+  providers: [],
   bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     NavbarComponent,
+    StoreCardComponent,
     FiltersComponent,
-  ],
+  ]
 })
-export class AppModule {}
+export class AppModule { }
