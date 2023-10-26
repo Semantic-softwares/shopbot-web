87abc00a64bb66db2529070ab7eaf72cccd79ce4
kizitech <kizitoohani@gmail.com>
2023-10-26 10:56:31 +0100

    Resolved comments

--- src/app/shared/components/category-card/category-card.component.ts
+++ src/app/shared/components/category-card/category-card.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { RouterExtensions } from 'nativescript-angular';
-// import { TextField } from 'ui/text-field';
-// import { EventData } from 'data/observable';
-// import { ActivatedRoute } from '@angular/router';
 
 @Component({
 	selector: 'category-card',
