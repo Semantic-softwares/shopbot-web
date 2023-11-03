3adf890a403bcc29e677bf09bb74e84c26b5a6a2
Alex Obogbare <aleobogb@WKMZTC406709.global.publicisgroupe.net>
2023-11-03 17:33:49 +0400

    fix module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {path: '', loadChildren: () => import('./home/home.routing').then(mod => mod.HOME_ROUTES)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
