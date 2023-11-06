f450098cab796b02dadf4802dabf0b99b2be7afa
kizitech <kizitoohani@gmail.com>
2023-11-06 14:32:54 +0100

    combined component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { HOME_ROUTES } from './home.routing';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BannerComponent } from '../shared/components/banner/banner.component';
 import { FiltersComponent } from '../shared/components/filters/filters.component';
+import { StoreCardComponent } from '../shared/components/store-card/store-card.component';
+import { CategoryCardComponent } from '../shared/components/category-card/category-card.component';
 
 @Component({
 	selector: 'home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.scss'],
 	standalone: true,
-	imports: [FiltersComponent]
+	imports: [ 
+		CommonModule,
+		FlexLayoutModule, 
+		BannerComponent, 
+		FiltersComponent, 
+		StoreCardComponent, 
+		CategoryCardComponent
+	]
 })
 
 export class HomeComponent implements OnInit {
--- src/app/shared/components/banner/banner.component.ts
+++ src/app/shared/components/banner/banner.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-//import { RouterExtensions } from 'nativescript-angular';
-//import { TextField } from 'ui/text-field';
-//import { EventData } from 'data/observable';
-//import { ActivatedRoute } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from '@angular/material/icon';
 
