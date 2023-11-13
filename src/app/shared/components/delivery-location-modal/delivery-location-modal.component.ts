4845ed56b25edb03c371e8af279fb26b09cfd710
Travisken <123268134+Travisken@users.noreply.github.com>
2023-11-13 14:20:39 +0100

    updated component

--- src/app/shared/components/delivery-location-modal/delivery-location-modal.component.ts
+++ src/app/shared/components/delivery-location-modal/delivery-location-modal.component.ts
@@ -6,10 +6,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { LocationModalComponent } from '../location-modal/location-modal.component';
-
-
 
 @Component({
   selector: 'delivery-location-modal',
@@ -24,16 +22,11 @@ import { LocationModalComponent } from '../location-modal/location-modal.compone
   ],
 })
 export class DeliveryLocationModalComponent {
-
-  constructor(public dialog: MatDialog) {}
-	closeDialog() {
-		this.dialog.closeAll();
-		
-	  }
-
-	openSecondDialog() {
-		this.dialog.open(LocationModalComponent);
-	  }
+  constructor(public dialog: MatDialog) { }
+  closeDialog() {
+    this.dialog.closeAll();
+  }
+  openSecondDialog() {
+    this.dialog.open(LocationModalComponent);
+  }
 }
-
-
