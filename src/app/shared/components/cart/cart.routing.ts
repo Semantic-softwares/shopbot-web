13c64021e2607f96bde983a63523ceb7b8118496
kizitech <kizitoohani@gmail.com>
2023-11-13 14:12:48 +0100

    fixed comment on files

--- src/app/shared/components/cart/cart.routing.ts
+++ src/app/shared/components/cart/cart.routing.ts
@@ -2,5 +2,5 @@ import { Route } from "@angular/router";
 import { CartComponent } from "./cart.component";
 
 export const CART_ROUTES: Route[] = [
-    {path: 'cart', component: CartComponent},
-  ];
+  {path: 'cart', component: CartComponent},
+];
