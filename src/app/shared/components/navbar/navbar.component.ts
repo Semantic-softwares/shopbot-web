3e241b6a96771f737b410066f8151c9ebdba674e
Travisken <123268134+Travisken@users.noreply.github.com>
2023-11-15 15:03:12 +0100

    link update on the toolbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import {MatBadgeModule} from '@angular/material/badge';
+import { MatBadgeModule } from '@angular/material/badge';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
@@ -11,7 +12,14 @@ import {MatBadgeModule} from '@angular/material/badge';
 	templateUrl: './navbar.component.html',
 	styleUrls: ['./navbar.component.scss'],
 	standalone: true,
-	imports: [MatToolbarModule, MatIconModule, FlexLayoutModule, MatButtonModule, MatBadgeModule]
+	imports: [
+		MatToolbarModule,
+		MatIconModule,
+		FlexLayoutModule,
+		MatButtonModule,
+		MatBadgeModule,
+		RouterModule
+	]
 })
 
 export class NavbarComponent implements OnInit {
