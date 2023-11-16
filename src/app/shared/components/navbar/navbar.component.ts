bb9f4018bfeb919e8459726eb6e89c9bd8baa41f
Travisken <123268134+Travisken@users.noreply.github.com>
2023-11-16 11:44:02 +0100

    updated modal

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -4,20 +4,26 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {MatBadgeModule} from '@angular/material/badge';
-import { RouterModule } from '@angular/router';
-
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { DeliveryLocationModalComponent } from '../delivery-location-modal/delivery-location-modal.component';
 
 @Component({
 	selector: 'navbar',
 	templateUrl: './navbar.component.html',
 	styleUrls: ['./navbar.component.scss'],
 	standalone: true,
-	imports: [MatToolbarModule, MatIconModule, FlexLayoutModule, MatButtonModule, MatBadgeModule, RouterModule]
+	imports: [MatToolbarModule, MatIconModule, FlexLayoutModule, MatButtonModule, MatBadgeModule, MatDialogModule]
+
 })
 
 export class NavbarComponent implements OnInit {
-
-	constructor() { }
-
+  constructor(public dialog: MatDialog) { }
+  closeDialog() {
+    this.dialog.closeAll();
+  }
+  
+  openDialog() {
+    this.dialog.open(DeliveryLocationModalComponent,  {width: '32rem', height: '28rem'});
+  }
 	ngOnInit() { }
 }
