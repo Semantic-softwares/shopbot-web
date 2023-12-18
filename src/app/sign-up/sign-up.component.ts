946a4d683b604d7c97c7be622279872e4d31452f
Travisken <123268134+Travisken@users.noreply.github.com>
2023-12-18 22:28:40 +0100

    force push due to errors and angular update
    code stash

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -5,6 +5,9 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-sign-up',
@@ -15,11 +18,45 @@ import { RouterModule } from '@angular/router';
     MatFormFieldModule, 
     MatInputModule, 
     MatIconModule,
-    RouterModule
+    RouterModule, 
   ],
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit{
+  hide = true;
 
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    
+  }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+
+  initForm() {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', [Validators.required, Validators.minLength(6)]],
+      country: ['', Validators.required],
+      currency: ['', Validators.required],
+      currencyCode: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+  
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+
+      console.log(this.registerForm.value);
+    } else {
+
+      console.log("Form is invalid. Please check the fields.");
+    }
+  }
 }
